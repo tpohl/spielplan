f15f2a164142c7d3a5b5643f29098367d49d77f7
Thorsten Pohl <thpohl@gmail.com>
2016-03-10 21:29:10 +0100

    Removed lombok.

--- src/main/java/com/thorstiland/spielplan/model/Team.java
+++ src/main/java/com/thorstiland/spielplan/model/Team.java
@@ -1,8 +1,24 @@
 package com.thorstiland.spielplan.model;
 
-import lombok.Data;
+import java.util.UUID;
 
-@Data
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Team {
+	@Id
+	@Column(name = "id")
+	private UUID id;
+	
 	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
