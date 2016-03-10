989fbce29d55b01fc5e9e1c06bce2d751f20753a
Thorsten Pohl <thpohl@gmail.com>
2016-03-10 22:33:44 +0100

    Added Hibernate Mapper to avoid lazy init stuff.

--- src/main/java/com/thorstiland/spielplan/rest/REST.java
+++ src/main/java/com/thorstiland/spielplan/rest/REST.java
@@ -3,23 +3,31 @@ package com.thorstiland.spielplan.rest;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.hibernate4.Hibernate4Module;
+
 import io.swagger.jaxrs.config.BeanConfig;
 
 /**
  * Offer REST interface.
+ * 
  * @author Thorsten Pohl
  */
 @ApplicationPath("rest")
 public class REST extends Application {
 
-    public REST() {
+	public REST() {
 
-        BeanConfig beanConfig = new BeanConfig();
-        beanConfig.setVersion("0.0.1");
-        beanConfig.setSchemes(new String[] { "http" , "https"});
-        beanConfig.setBasePath("/rest");
-        beanConfig.setResourcePackage("com.thorstiland.spielplan.rest");
-        beanConfig.setScan(true);
-    }
+		BeanConfig beanConfig = new BeanConfig();
+		beanConfig.setVersion("0.0.1");
+		beanConfig.setSchemes(new String[] { "http", "https" });
+		beanConfig.setBasePath("/rest");
+		beanConfig.setResourcePackage("com.thorstiland.spielplan.rest");
+		beanConfig.setScan(true);
+
+		ObjectMapper mapper = new ObjectMapper();
+		// for Hibernate 4.x:
+		mapper.registerModule(new Hibernate4Module());
+	}
 
 }
