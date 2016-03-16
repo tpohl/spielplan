5cec6ffe59b7c2a5d36ac54c9103e526332ee76f
Thorsten Pohl <thpohl@gmail.com>
2016-03-16 21:52:45 +0100

    Adding a player to a community does not modify the player.

--- src/main/java/com/thorstiland/spielplan/service/CommunityService.java
+++ src/main/java/com/thorstiland/spielplan/service/CommunityService.java
@@ -2,27 +2,33 @@ package com.thorstiland.spielplan.service;
 
 import java.util.ArrayList;
 
+import javax.inject.Inject;
 import javax.inject.Named;
 
 import com.thorstiland.spielplan.model.Community;
 import com.thorstiland.spielplan.model.Team;
 
 @Named
-public class CommunityService extends CrudService<Community>{
+public class CommunityService extends CrudService<Community> {
+
+	@Inject
+	TeamService teamService;
 
 	public CommunityService() {
 		super(Community.class);
 	}
 
 	public Community addTeam(Community c, Team team) {
-		team = this.entityManager.merge(team);
-		if (c.getTeams() == null){
+
+		team = teamService.find(team.getId());
+
+		if (c.getTeams() == null) {
 			c.setTeams(new ArrayList<>(1));
 		}
 		c.getTeams().add(team);
 		this.merge(c);
 		return c;
-		
+
 	}
 
 }
