deb23db9c453445fd06a2a7eafcbde45ebdb8d6b
Thorsten Pohl <thpohl@gmail.com>
2016-10-07 23:02:44 +0200

    SeasonDTO no longer has a Community.

--- dto/src/main/java/com/thorstiland/spielplan/dto/SeasonDto.java
+++ dto/src/main/java/com/thorstiland/spielplan/dto/SeasonDto.java
@@ -8,7 +8,6 @@ import lombok.Data;
 public class SeasonDto {
 	private long id;
 	private String name;
-	private CommunityDto community;
 	private List<TeamDto> teams;
 	private List<GameDto> games;
 }
